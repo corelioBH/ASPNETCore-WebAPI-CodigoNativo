0ca14cb048e2dcf34d918074351d1aee5cf64caf
Marco Mendes <corelio@gmail.com>
2017-03-01 01:54:51 -0300

    Importacao inicial do projeto

---
+++ src/BibliotecaReusavel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using System;
+namespace BibliotecaReusavel
+{
+    public class FachadaCodigoNativo
+    {
+
+        // Import user32.dll (containing the function we need) and define
+        // the method corresponding to the native function.
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+
+
+        public static void Main(string[] args)
+        {
+            // Invoke the function as a regular managed method.
+            MessageBox(IntPtr.Zero, "Command-line message box", "Attention!", 0);
+
+        }
+    }
+
+}
